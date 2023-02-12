69030ab4e7f3d1ebf490990f0d8871e6f686c04f
Mohamed El-Seady <96991597+Mandela95@users.noreply.github.com>
2023-02-12 13:56:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,12 @@
-let title = document.querySelector(".title");
+let inputTitle = document.querySelector(".title");
 let article = document.querySelector(".article");
 let author = document.querySelector(".author");
-let btn = document.querySelector("button");
+let $publishBtn = document.querySelector("button");
 let tasks = document.querySelector(".tasks");
 
-btn.onclick = function () {
-  if (title.value && article.value && author.value !== "") {
-    let titleDiv = document.createElement("div");
-    let task = document.createElement("p");
-    task.classList = "content";
-    let titleValue = title.value;
-    titleDiv.classList = "task";
-    title.value = "";
-
-    let articleDiv = document.createElement("p");
-    articleDiv.classList = "addedArticle";
-    articleDiv.innerHTML = article.value;
-    article.value = "";
-
-    let authorDiv = document.createElement("p");
-    let date = document.createElement("span");
-    authorDiv.classList = "addedAuthor";
-    authorDiv.innerHTML = author.value;
-    date.innerHTML = new Date().toDateString();
-    author.value = "";
-
-    let links = document.createElement("div");
-    let edit = document.createElement("a");
-    let del = document.createElement("a");
-
-    links.classList = "action";
-    edit.classList = "edit";
-    edit.innerHTML = "Edit";
-
-    del.classList = "delete";
-    del.innerHTML = "Delete";
-
-    task.append(titleValue);
-    links.append(edit, del);
-    titleDiv.append(task, links, articleDiv, authorDiv, date);
-    tasks.append(titleDiv);
+$publishBtn.onclick = function () {
+  if (inputTitle.value && article.value && author.value !== "") {
+    renderArticle(inputTitle, article, author);
   } else {
     alert("Please Fill All Fields");
   }
@@ -48,16 +15,16 @@ btn.onclick = function () {
 tasks.addEventListener("click", (e) => {
   target = e.target;
   if (target.classList.contains("delete")) {
-    document.querySelector(".task").remove();
+    target.parentElement.parentElement.remove();
   }
 });
 
-tasks.addEventListener("click", (e) => {
-  target = e.target;
-  if (target.classList.contains("edit")) {
-    tasks = target.parentElement.parentElement;
-    title.value = tasks.children[0].textContent;
-    article.value = tasks.children[2].textContent;
-    author.value = tasks.children[3].textContent;
-  }
-});
+// tasks.addEventListener("click", (e) => {
+//   target = e.target;
+//   if (target.classList.contains("edit")) {
+//     tasks = target.parentElement.parentElement;
+//     title.value = tasks.children[0].textContent;
+//     article.value = tasks.children[2].textContent;
+//     author.value = tasks.children[3].textContent;
+//   }
+// });
