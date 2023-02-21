602e7896fbec20821b2eae1919ecba82fcf0a276
Mohamed El-Seady <96991597+Mandela95@users.noreply.github.com>
2023-02-21 00:59:41 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,73 @@
 let inputTitle = document.querySelector(".title");
-let article = document.querySelector(".article");
-let author = document.querySelector(".author");
-let $publishBtn = document.querySelector("button");
+let articleContent = document.querySelector(".article");
+let authorName = document.querySelector(".author");
+let $publishBtn = document.querySelector(".publishBtn");
 let tasks = document.querySelector(".tasks");
 
+let titleRequired = document.getElementById("titleRequired");
+let textareaRequired = document.getElementById("textareaRequired");
+let authorRequired = document.getElementById("authorRequired");
+
+let mood = "publish";
+let assistantVar;
+
 $publishBtn.onclick = function () {
-  if (inputTitle.value && article.value && author.value !== "") {
-    renderArticle(inputTitle, article, author);
+  let newArticle = {
+    title: inputTitle.value,
+    article: articleContent.value,
+    author: authorName.value,
+  };
+  if (mood === "publish") {
+    if (
+      inputTitle.value != "" &&
+      articleContent.value != "" &&
+      authorName.value != ""
+    ) {
+      allArticles.push(newArticle);
+      clearData();
+    } else {
+      if (inputTitle.value != "") {
+        titleRequired.style.display = "none";
+      } else {
+        titleRequired.style.display = "block";
+        titleRequired.innerHTML = "Title Is Required";
+
+        setTimeout(
+          () => document.querySelector("#titleRequired").remove(),
+          1000
+        );
+      }
+      if (articleContent.value != "") {
+        textareaRequired.style.display = "none";
+      } else {
+        textareaRequired.style.display = "block";
+        textareaRequired.innerHTML = "Article Content Is Required";
+
+        setTimeout(
+          () => document.querySelector("#textareaRequired").remove(),
+          2000
+        );
+      }
+      if (authorName.value != "") {
+        authorRequired.style.display = "none";
+      } else {
+        authorRequired.style.display = "block";
+        authorRequired.innerHTML = "Author Name Is Required";
+
+        setTimeout(
+          () => document.querySelector("#authorRequired").remove(),
+          3000
+        );
+      }
+    }
   } else {
-    alert("Please Fill All Fields");
+    allArticles[assistantVar] = newArticle;
+    mood = "publish";
+    $publishBtn.innerHTML = "Publish";
+    clearData();
   }
+
+  localStorage.setItem("article", JSON.stringify(allArticles));
+
+  showData();
 };
-
-tasks.addEventListener("click", (e) => {
-  target = e.target;
-  if (target.classList.contains("delete")) {
-    target.parentElement.parentElement.remove();
-  }
-});
-
-// tasks.addEventListener("click", (e) => {
-//   target = e.target;
-//   if (target.classList.contains("edit")) {
-//     tasks = target.parentElement.parentElement;
-//     title.value = tasks.children[0].textContent;
-//     article.value = tasks.children[2].textContent;
-//     author.value = tasks.children[3].textContent;
-//   }
-// });
