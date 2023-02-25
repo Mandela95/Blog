1e1cfd7fe73fd5bf283647ebfeb31732f55408e0
Mohamed El-Seady <96991597+Mandela95@users.noreply.github.com>
2023-02-25 14:30:13 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,98 @@
-let inputTitle = document.getElementById("title");
-let articleContent = document.getElementById("article");
-let authorName = document.getElementById("author");
-let $publishBtn = document.querySelector(".publishBtn");
-let tasks = document.querySelector(".tasks");
+let $inputTitle = document.getElementById("title");
+let $articleContent = document.getElementById("article");
+let $authorName = document.getElementById("author");
 
-let titleRequired = document.getElementById("titleRequired");
-let textareaRequired = document.getElementById("textareaRequired");
-let authorRequired = document.getElementById("authorRequired");
+let inputs = [$inputTitle, $articleContent, $authorName];
+
+let $publishBtn = document.getElementById("publishBtn");
+let $articles = document.getElementById("articles");
+
+let $titleRequired = document.getElementById("titleRequired");
+let $textareaRequired = document.getElementById("textareaRequired");
+let $authorRequired = document.getElementById("authorRequired");
 
 let mood = "publish";
-let assistantVar;
+let altIndex;
 
-$publishBtn.onclick = function () {
-  let newArticle = {
-    title: inputTitle.value,
-    article: articleContent.value,
-    author: authorName.value,
-  };
+const publish = function () {
   if (mood === "publish") {
     if (
-      inputTitle.value != "" &&
-      articleContent.value != "" &&
-      authorName.value != ""
+      $inputTitle.value.trim() != "" &&
+      $articleContent.value.trim() != "" &&
+      $authorName.value.trim() != ""
     ) {
+      let newArticle = {
+        title: $inputTitle.value,
+        article: $articleContent.value,
+        author: $authorName.value,
+      };
       allArticles.push(newArticle);
-      clearData();
+      clearArticles();
     } else {
-      if (inputTitle.value != "") {
-        titleRequired.style.display = "none";
+      if ($inputTitle.value.trim() != "") {
+        $titleRequired.style.display = "none";
       } else {
-        titleRequired.style.display = "block";
-        titleRequired.innerHTML = "Title Is Required";
+        $titleRequired.style.display = "block";
       }
-      if (articleContent.value != "") {
-        textareaRequired.style.display = "none";
+      if ($articleContent.value.trim() != "") {
+        $textareaRequired.style.display = "none";
       } else {
-        textareaRequired.style.display = "block";
-        textareaRequired.innerHTML = "Article Content Is Required";
+        $textareaRequired.style.display = "block";
       }
-      if (authorName.value != "") {
-        authorRequired.style.display = "none";
+      if ($authorName.value.trim() != "") {
+        $authorRequired.style.display = "none";
       } else {
-        authorRequired.style.display = "block";
-        authorRequired.innerHTML = "Author Name Is Required";
+        $authorRequired.style.display = "block";
       }
     }
   } else {
-    allArticles[assistantVar] = newArticle;
+    let newArticle = {
+      title: $inputTitle.value,
+      article: $articleContent.value,
+      author: $authorName.value,
+    };
+    allArticles[altIndex] = newArticle;
     mood = "publish";
     $publishBtn.innerHTML = "Publish";
-    clearData();
+    clearArticles();
   }
 
-  localStorage.setItem("article", JSON.stringify(allArticles));
+  localStorage.setItem("articles", JSON.stringify(allArticles));
 
-  showData();
+  renderArticles();
 };
+
+$publishBtn.onclick = publish;
+
+$inputTitle.addEventListener("input", function () {
+  if ($inputTitle.value.trim() != "") {
+    $titleRequired.style.display = "none";
+  } else {
+    $titleRequired.style.display = "block";
+  }
+});
+
+$articleContent.addEventListener("input", function () {
+  if ($articleContent.value.trim() != "") {
+    $textareaRequired.style.display = "none";
+  } else {
+    $textareaRequired.style.display = "block";
+  }
+});
+
+$authorName.addEventListener("input", function () {
+  if ($authorName.value.trim() != "") {
+    $authorRequired.style.display = "none";
+  } else {
+    $authorRequired.style.display = "block";
+  }
+});
+
+// press enter
+inputs.map((item) => {
+  item.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+      publish();
+    }
+  });
+});
